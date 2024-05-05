211752127cce43c4e9d29089c2b0f29ff2ac1f00
arash <arash@Arash.arash>
2024-05-05 16:28:31 -0400

    init 1.5.2.1, field_utils.js is fixed

--- __manifest__.py
+++ __manifest__.py
@@ -16,7 +16,7 @@
     # for the full list
     'category': 'Service Desk/Service Desk',
     'application': False,
-    'version': '1.2.5',
+    'version': '1.2.5.1',
 
     # any module necessary for this one to work correctly
     'depends': ['base', 'web'],
@@ -27,25 +27,27 @@
         ],
     'assets': {
         'web._assets_common_scripts': [
-            'odoo_jalaali/static/js/jalaali.js',
-            'odoo_jalaali/static/js/moment/moment-jalaali.js',
-            'odoo_jalaali/static/js/tempusdominus/tempusdominus_fixed.js',
-            ('replace', 'web/static/lib/owl/owl.js', 'odoo_jalaali/static/js/owl/owl_fixed.js'),
+            # 'odoo_jalaali/static/src/js/field_utils.js',
+            'odoo_jalaali/static/src/js/jalaali.js',
+            'odoo_jalaali/static/src/js/moment/moment-jalaali.js',
+            'odoo_jalaali/static/src/js/tempusdominus/tempusdominus_fixed.js',
+            ('replace', 'web/static/lib/owl/owl.js', 'odoo_jalaali/static/src/js/owl/owl_fixed.js'),
         ],
         'web._assets_common_styles': [
-            'odoo_jalaali/static/css/fonts_fn.scss',
+            'odoo_jalaali/static/src/css/fonts_fn.scss',
             # 'odoo_jalaali/static/css/fonts_en.scss',
         ],
         'web.assets_backend': [
-            'odoo_jalaali/static/css/fonts_web.scss',
+            'odoo_jalaali/static/src/css/fonts_web.scss',
+            ('replace', 'web/static/src/legacy/js/fields/field_utils.js', 'odoo_jalaali/static/src/js/field_utils.js'),
             ],
         'web.assets_frontend': [
-            'odoo_jalaali/static/css/fonts_front.scss'
+            'odoo_jalaali/static/src/css/fonts_front.scss'
             ],
         'web.report_assets_common': [
-            'odoo_jalaali/static/css/fonts_fn.scss',
+            'odoo_jalaali/static/src/css/fonts_fn.scss',
             # 'odoo_jalaali/static/css/fonts_en.scss',
-            'odoo_jalaali/static/css/fonts_report.scss',
+            'odoo_jalaali/static/src/css/fonts_report.scss',
             ],
         },
     'license': 'LGPL-3',
